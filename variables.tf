009d3d700680f7b8de89864d42e43b7c0ba9dd58
Davi <davi.carneiro@o2b.com.br>
2022-03-18 19:25:58 -0300

    Create Codigos automaçao google gcp

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "project_id" {
+  type    = string
+  default = "jovial-sound-343419"
+}
+
+variable "regiao" {
+  type    = string
+  default = "southamerica-east1-b"
+}
+
+variable "nome" {
+  type    = string
+  default = "vm-webserver"
+}
+
+variable "tipo_maquina" {
+  type    = string
+  default = "f1-micro"
+}
+
+variable "zona" {
+  type    = string
+  default = "southamerica-east1-b"
+}
+
+variable "imagem" {
+  type    = string
+  default = "debian-cloud/debian-9"
+}
